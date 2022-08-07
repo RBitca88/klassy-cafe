73c40d62256507e1eaaac52084908cc82a2e1cb0
RBitca88 <radubitca4@gmail.com>
2022-08-07 16:41:03 +0300

    Creating admin login system

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function index() {
         return view("home");
+    }
+
+    public function redirects() {
+
+        $usertype = Auth::user() -> usert_type;
+
+        if($usertype == '1'){
+            return view('admin.admin-home');
+        } else {
+            return view('home');
+        }
+
     }
 }
