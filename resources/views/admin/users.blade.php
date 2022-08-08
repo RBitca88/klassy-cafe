7f66c5929f5d350cb85ff6f633d5ec6475e80957
RBitca88 <radubitca4@gmail.com>
2022-08-08 13:29:20 +0300

    Display an d delete users from admin

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+   
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include("admin.admin-css")
+  </head>
+  <body>
+    <div class="container-scroller">
+    
+         @include("admin.nav-bar")
+
+         <div style="position: relative; top: 60px; right: -200px">
+            <table bgcolor="grey"; border="3px">
+                <tr>
+                    <th style="padding: 50px">Name</th>
+                    <th style="padding: 50px">Email</th>
+                    <th style="padding: 50px">Action</th>
+                </tr>
+
+                @foreach($data as $data)
+                <tr align="center">
+                    <td>{{$data -> name}}</td>
+                    <td>{{$data -> email}}</td>
+
+                    @if($data -> usert_type == "0")
+                    <td><a href="{{url('/deleteuser', $data -> id)}}">Delete</a></td>
+                    @else
+                    <td><a>Not allowed</a></td>
+                    @endif
+                </tr>
+                @endforeach
+                
+            </table>
+         </div>
+
+    </div>     
+   
+    @include("admin.admin-script")
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +19,14 @@ use App\Http\Controllers\HomeController;
 
 Route::get("/", [HomeController::class, "index"]);
 
-Route::get("/redirects", [HomeController::class, "redirects"]);
+Route::get("/users", [AdminController::class, "user"]);
 
+Route::get("/deleteuser/{id}", [AdminController::class, "deleteuser"]);
+
+
+
+
+Route::get("/redirects", [HomeController::class, "redirects"]);
 
 Route::middleware([
     'auth:sanctum',
